67ab10edfbf85a9974d993fd3df7d13b2006f552
zhe00a-vc <Bangyou.Zheng@csiro.au>
2016-05-13 21:25:32 +1000

    remove _bookdown_files

--- generate-gh-pages.R
+++ generate-gh-pages.R
@@ -1,5 +1,6 @@
 Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc")
 Sys.setenv(RMARKDOWN_MATHJAX_PATH="C:/Program Files/RStudio/resources/mathjax-23/MathJax.js")
 library(bookdown)
+unlink('_bookdown_files', recursive = TRUE)
 
 render_book('.')
