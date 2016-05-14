1f390c6b7867294ca80b9aaef3abcd8c9c423515
zhe00a-vc <Bangyou.Zheng@csiro.au>
2016-05-15 09:57:45 +1000

    Test travis

--- _generate-gh-pages.R
+++ _generate-gh-pages.R
@@ -3,4 +3,6 @@ Sys.setenv(RMARKDOWN_MATHJAX_PATH="C:/Program Files/RStudio/resources/mathjax-23
 library(bookdown)
 unlink('_bookdown_files', recursive = TRUE)
 
-tryCatch(bookdown::render_book('.'), error= function(e) stop(e))
+tryCatch(bookdown::render_book('.'), error = function(e) {
+    quit(save = "no", status = 100, runLast = FALSE)
+    })
