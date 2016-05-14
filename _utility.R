3b697e4de3a278d801d1964129ae4c5db636f1ce
zhe00a-vc <Bangyou.Zheng@csiro.au>
2016-05-15 08:39:09 +1000

    Update leaf page and others.

--- _utility.R
+++ _utility.R
@@ -46,6 +46,53 @@ plot_report <- function(df, x_var, y_cols, x_lab = x_var, y_lab = 'Value',
 
 
 
+
+plot_report_vector <- function(df, x_var, y_cols, x_lab = x_var, y_lab = 'Value',
+                        panel = FALSE) {
+    library(ggplot2)
+    x_var_name <- gsub('.*\\.(.*)', '\\1', x_var)
+    x_var_n <- list()
+    x_var_n[[x_var_name]] <- x_var
+
+    col_names <- grepl(y_cols, names(df)) | (names(df) %in% x_var)
+    pd <- df[,col_names]
+    names(pd) <- gsub('\\:|\\(|\\)', '_', names(pd))
+
+    y_cols_new <- names(pd)[!(names(pd) %in% x_var)]
+
+    pd <- pd %>%
+        gather_(key_col = 'Trait', value_col = 'Value', gather_cols = y_cols_new) %>%
+        mutate(
+            Trait = gsub('.*\\.(.*)', '\\1', Trait),
+            Index = gsub('(.*)\\d+_\\d+_(\\d+)_', '\\2', Trait),
+            Trait = gsub('(.*)\\d+_\\d+_(\\d+)_', '\\1', Trait),
+            Index = factor(as.numeric(as.character(Index)))
+            ) %>%
+        rename_(.dots = x_var_n)
+    #
+    #     ggvis(pd, x = as.name(x_var_name), y = ~Value, stroke = ~Trait) %>%
+    #         layer_lines() %>%
+    #         add_legend("stroke", title = "") %>%
+    #         add_tooltip(function(df) paste0('Stage: ', df[[x_var_name]], '\nValue: ', df$Value))
+    #
+    p <- ggplot(pd) +
+        geom_line(aes_string(x_var_name, 'Value', colour = 'Index')) +
+        geom_point(aes_string(x_var_name, 'Value', colour = 'Index')) +
+        theme_bw() +
+        theme(legend.position = 'bottom') +
+        xlab(x_lab) + ylab(y_lab) +
+        guides(colour = guide_legend(title = ''))
+    if (length(grep('Stage', x_var)) > 0) {
+        p <- p +
+            scale_x_continuous(breaks = seq(1, 10))
+    }
+    p
+}
+
+
+
+
+
 plot_xypair <- function(pmf, xpath, x_lab, y_lab) {
 
     xypair <- xml_find_one(pmf, xpath = paste0(xpath, '/following-sibling::XYPairs'))
